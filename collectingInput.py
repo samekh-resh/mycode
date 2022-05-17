b98b0a59a7fa0fc287a9389a5b14faa43d659171
Tiamat <samekh.resh@gmail.com>
2022-05-17 14:13:26 +0000

    made a file to collect input and a file to simply play around, learned how to enter the python playgroun o the terminal, trying to figure out how to do that with java

---
+++ collectingInput.py
@@ -0,0 +1,13 @@
+def main():
+    userName = input("hey, what's your name? ")
+    userDay = input("wait hey, quick question... what's the day of the week?" )
+
+    print( "hey, " + userName + "! Happy  " + userDay + "!")
+
+
+
+
+
+
+
+main()
