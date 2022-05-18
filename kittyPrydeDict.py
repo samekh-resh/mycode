d6610a6294b74526fbd0f321c91965465ab99a89
Tiamat <samekh.resh@gmail.com>
2022-05-18 17:29:27 +0000

    made some changes to calculator and worked on dictionaries

---
+++ kittyPrydeDict.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+kitty = {"Real Name": "Katherine Anne Pryde",
+        "Creators": ["Chris Claremont","John Byrne"],
+        "Aliases" : ["Shadowcat", "Ariel", "Sprite", "Star-Lord (Star-Lady)", "Captain Kate Pryde", "Red Queen"],
+        "Powers":"Phasing",
+        "SAA": {"skills": "Ninja Training", "experience": ["Fighting Experience", " Leadership Experience"]}
+        }
+
+
+print(kitty["Creators"][0])
+
+
+
