eb5f032b32ae0648d303822be6c77c2f7895697e
Tiamat <samekh.resh@gmail.com>
2022-05-19 16:33:56 +0000

    if else playing

---
+++ iftest/condition02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mtg":
+    print("The hostname was found to be MTG")
+else:
+    print("not the right input")
+    print("closing project")
